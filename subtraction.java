094c86afb5b03a0f585f389c794e96e72869a1d9
Srilekha <149182862+Srilekhanalabolu@users.noreply.github.com>
2024-04-20 15:44:16 +0530

    Add files via upload

---
+++ subtraction.java
@@ -0,0 +1,11 @@
+package arithmeticop;
+public class subtraction{
+           private int d1,d2;
+           public subtraction(int n1,int n2){
+                this.d1=n1;
+                this.d2=n2;
+      }
+      public void difference(){
+                 System.out.println("subtraction of two given numbers is"+(d1-d2));
+           }
+}
