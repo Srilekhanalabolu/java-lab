8795016a6c76be788d9250a87eae6b39878a53e2
Srilekha <149182862+Srilekhanalabolu@users.noreply.github.com>
2024-04-27 15:13:57 +0530

    Add files via upload

---
+++ favsuperhero.java
@@ -0,0 +1,6 @@
+class favsuperhero{
+            public static void main(String[] args){
+                    Ironman im=new Ironman("Iron man","fly","Jovial",55,"avengers");
+                    im.display();
+                   }
+              }
